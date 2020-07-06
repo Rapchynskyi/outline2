654969f3c62bb468e42435b36f5a2c79ae9addf1
Rapchynskyi <67628123+Rapchynskyi@users.noreply.github.com>
2020-07-06 08:56:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
+
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
-                 
-let img = new Image();
-img.src = "img2.png";
 
+let img = new Image();
+img.src = "pic.jpg";  
+let x;
+let w;
+let h;
+let imageData;
 img.onload = function() {
   canvas.width=img.width;
   canvas.height=img.height;
   context.drawImage(img, 0, 0);
-  let imageData = context.getImageData(0,0, img.width, img.height);
-  console.log(imageData.data);
-  
-  let x = imageData.data; 
+  imageData = context.getImageData(0,0, img.width, img.height);
+   x = imageData.data; 
   let p = 0;
-  let w = img.width*4;
-  let h = img.height;
-
+   w = img.width*4;
+   h = img.height;
+};
  
+  
 
+ function func(){
   function NearlyBlack(pos){
     if(x[pos]<150 && x[pos+1]<150 && x[pos+2]<150) return true;
     return false;
   }
   function NearlyWhite(pos){
-    return !IsBlack(pos);
+    return !NearlyBlack(pos);
   }
   function MakeBlack(pos){
     x[pos] = 0;
@@ -40,19 +44,15 @@ img.onload = function() {
     MakeWhite(q);
   }*/
   
- 
+     let white = 0, black = 0;
     for(let t = 0; t<parseInt(x.length);t+=4){
-     if(NearlyBlack(t) || ((NearlyBlack(t-w) + NearlyBlack(t+w) + NearlyBlack(t+4) + NearlyBlack(t-4)) >=3)) {MakeBlack(t);}
-      else  MakeWhite(t);
+     if(NearlyBlack(t) || ((NearlyBlack(t-w) + NearlyBlack(t+w) + NearlyBlack(t+4) + NearlyBlack(t-4)) >=3)) {MakeBlack(t); black++;}
+      else  {MakeWhite(t); white++;}
     }
-  
-  
-  
-
-
+  if(white>black) console.log("white");
+   else console.log("black");
 
   //находим первый черный пиксель
-
 
   /*
   0 - восток
@@ -65,11 +65,11 @@ img.onload = function() {
   let cont = [];
   let j = 0;
   for(let t=0; t<w*h;t+=4){
-    let amountBlack = NearlyBlack(t-w-4) + NearlyBlack(t-w+4) + NearlyBlack(t+w-4) + NearlyBlack(t+w+4)+ 
-    NearlyBlack(t-w) + NearlyBlack(t+w) + NearlyBlack(t+4) + NearlyBlack(t-4);
-   // let amountWhite = NearlyWhite(t-w-4) + NearlyWhite(t-w+4) + NearlyWhite(t+w-4) + NearlyWhite(t+w+4)+ 
-  //  NearlyWhite(t-w) + NearlyWhite(t+w) + NearlyWhite(t+4) + NearlyWhite(t-4);
-    if((amountBlack >=2) && (amountBlack <=6)) cont [j++] = t;
+    let amountBlack = NearlyBlack(t - w- 4) + NearlyBlack(t - w + 4) + NearlyBlack(t + w - 4) + NearlyBlack(t + w + 4) + 
+    NearlyBlack(t - w) + NearlyBlack(t + w) + NearlyBlack(t + 4) + NearlyBlack(t - 4);
+    let amountWhite = NearlyWhite(t - w - 4) + NearlyWhite(t - w + 4) + NearlyWhite(t + w - 4) + NearlyWhite(t + w + 4)+ 
+    NearlyWhite(t - w) + NearlyWhite(t + w) + NearlyWhite(t + 4) + NearlyWhite(t - 4);
+    if((amountWhite >=2) && (amountWhite <=6) && (amountBlack >=2) && (amountBlack <=6) && (!(t<w && black>white)) && (!(t>=(h*w-w) && black>white))) cont [j++] = t;
   }
   //алгоритм жука
   /*if(NearlyBlack(0)){
@@ -122,18 +122,21 @@ else {
 
 }*/
   //рисование контура 
-
+  console.log(cont);
+ 
   for(let j = 0; j < cont.length; j ++){
-   
-    if(cont[j]%(w)==0) cont[j]-=4;
+    if((cont[j]+4)%(w)==0) cont[j]-=4;
     x[cont[j]] = 255;
-    x[cont[j]+1] = 0;
-    x[cont[j]+2] = 0;
-      
+    x[cont[j]+1] = 68;
+    x[cont[j]+2] = 51;
+    console.log((cont[j]%w) + " " + Math.floor(cont[j]/w));
+   // console.log("-> "+(cont[j].toPrecision(3)%w)+ " " + cont[j]/w);
   }
+  
   console.log("FINISH!!!");
    context.putImageData(imageData, 0, 0);
 };
+
 
 
  
